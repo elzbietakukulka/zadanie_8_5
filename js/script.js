090d7139c0b4a5747937aaafb1df5a7962f3b151
Elżbieta Kukułka <ellakaras@gmail.com>
2018-02-21 17:53:55 +0100

    Update scripts.js, index.html

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter a random number');
+var h = prompt('Enter a random number');
+
+var triangleArea = a*h/2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
